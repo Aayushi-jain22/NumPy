8842c06446e6994359b953b2820ee36acc9234db
Aayushi-jain22 <91220758+Aayushi-jain22@users.noreply.github.com>
2026-07-19 19:22:40 +0530

    Learned about array numpy properties

---
+++ array_np_properties/dtype.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+arr = np.array([[1, "2", 3], [4, 5, 6]])
+print(arr.dtype)  # Output: float64 (or float32 depending on the system)
---
+++ array_np_properties/ndin.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+array_1d = np.array([1, 2, 3])
+print(array_1d.ndim)  # Output: 1
+
+array_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(array_2d.ndim)  # Output: 2   
+
+array_3d = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])   
+print(array_3d.ndim)  # Output: 3
